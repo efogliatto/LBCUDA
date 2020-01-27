82e1c74e75d8397961bef2d3cc988e6e3ea8a689
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2020-01-27 15:30:55 -0300

    Aplicacion para iterar sobre nodos, reducir en los vecinos y sumar estas reducciones

---
+++ src/applications/tests/nbReduction/nbReduction.c
@@ -0,0 +1,135 @@
+/*
+
+  nbReduction
+
+  Suma de componentes de funciones de distribuci\'on en cada vecino, y reduccion
+
+ */
+
+
+
+#include <latticeMesh.h>
+
+#include <basic.h>
+
+#include <stdio.h>
+
+
+int main(int argc, char** argv) {
+
+    
+    printf( "                    \n" );
+    printf( "     o-----o-----o  \n" );
+    printf( "     | -   |   - |  \n" );
+    printf( "     |   - | -   |                            nbReduction\n" );
+    printf( "     o<----o---->o  \n" );
+    printf( "     |   - | -   |  Operacion de reduccion sobre funcion de distribucion sobre vecinos\n" );
+    printf( "     | -   |   - |  \n" );
+    printf( "     o-----o-----o  \n\n" );
+
+
+    // Argumentos:
+    //
+    // - argv[1] = numero de iteraciones
+
+    uint nit = atoi( argv[1] );
+    
+
+
+    // Lectura de malla
+
+    basicMesh mesh = readBasicMesh();
+
+
+    // Alocacion de funcion de distribucion como arreglo unidimensional
+    //
+    // Si se desea acceder a los componentes de field usando dos indices,
+    // entonces puede hacerse algo como
+    //
+    // for( i = 0 ; i < mesh.nPoints ; i++)
+    //     for( j = 0 ; j < mesh.Q ; j++)
+    //         idx = i*mesh.Q + j;
+
+    uint fsize = mesh.nPoints * mesh.Q;
+    
+    scalar* field = (scalar*)malloc( fsize * sizeof(scalar) );
+
+
+    // Alocacion de arreglo de salida
+
+    scalar* sum = (scalar*)malloc( mesh.nPoints * sizeof(scalar) );
+
+
+    
+    // Inicializacion (puede ser otra)
+
+    for( uint i = 0 ; i < fsize ; i++ )
+	field[i] = i;
+
+
+
+    // Reduccion
+
+    printf(" Aplicando reducción en %d iteraciones\n\n", nit);
+
+    timeInfo Time;
+
+    startTime(&Time);
+
+    for( uint k = 0 ; k < nit ; k++ ) {
+
+
+	for( uint i = 0 ; i < mesh.nPoints ; i++ ) {
+
+	    sum[i] = 0;
+
+
+	    // Reduccion en cada vecino
+
+	    for( uint j = 0 ; j < mesh.Q ; j++ ) {
+
+		// El indice del nodo vecino es nb[i][j]
+		// Por lo tanto para iterar sobre sus componentes usando l es
+		//
+		//    for( uint l = 0 ; l < mesh.Q ; l++ )
+		//	nbsum += field[ mesh.nb[i][j]*mesh.Q + l  ];
+		//
+		
+		scalar nbsum = 0;
+		
+		if( mesh.nb[i][j] != -1 ) {
+
+		    for( uint l = 0 ; l < mesh.Q ; l++ ) {
+
+			nbsum += field[ mesh.nb[i][j]*mesh.Q + l  ];
+
+		    }
+
+		}
+
+		sum[i] += nbsum;
+
+
+	    }
+
+	}
+	
+
+    }
+
+
+    printf( "\n   Reduccion finalizada en %f segundos\n\n", elapsedTime(&Time) );
+
+
+
+    // Limpieza de memoria
+
+    free( field );
+
+    free( sum );
+
+    freeBasicMesh( &mesh );
+    
+    return 0;
+
+}
