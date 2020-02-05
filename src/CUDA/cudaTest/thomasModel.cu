c8ce845280049693c8b194fa0be36ebe3f3eaebb
Thomas Coronel <coronelth@mecom26.cabib.local>
2020-02-05 10:35:46 -0300

    Se optimizo la forma de realizar las reducciones, utilizando el formato de las placas , ya que poseen x,y,z

--- src/CUDA/cudaTest/thomasModel.cu
+++ src/CUDA/cudaTest/thomasModel.cu
@@ -10,20 +10,23 @@
 
 extern "C" __global__ void thomasModel(cuscalar* field, cuscalar* zeroth, int np, int Q ) {
 
-    int idx = threadIdx.x + blockIdx.x*blockDim.x;
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
 
-    int idy = threadIdx.y + blockIdx.y*blockDim.y;
+    int idy = threadIdx.y + blockIdx.y * blockDim.y;
+
+    int idz = threadIdx.z + blockIdx.z * blockDim.z;
+
 	
-    int j=0;	
+   // int j=0;	
    
     if( idx < np ) {
   	
 
     	cuscalar sum = 0;
 	if( idy < Q ) {
-    		for( j = 0 ; j < Q ; j++ ) {
+    		if( idz < Q ) {
 
-    	    	sum += field[ idx*Q + j ];
+    	    	sum += field[ idx*Q + idz ];
 
 		}
 
